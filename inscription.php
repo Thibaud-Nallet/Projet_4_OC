2f48271d834825c774dc6a04e8663b65380fd519
Thibaud-Nallet <thibaud.nallet@gmail.com>
2019-09-02 13:17:17 +0200

    Inscription dans la bdd fonctione

--- inscription.php
+++ inscription.php
@@ -1,4 +1,55 @@
-<?php $title = "Article | Billet simple pour l'Alaska"; ?>
+<?php 
+$title = "Article | Billet simple pour l'Alaska"; 
+
+$bdd = new PDO('mysql:host=localhost;dbname=jeanForteroche', 'root', 'root');
+
+function checkInput($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
+
+if(isset($_POST['formInscription'])) {
+   $inputPseudo = checkInput($_POST['inputPseudo']);
+   $inputMail = checkInput($_POST['inputMail']);
+   $inputPassword = checkInput($_POST['inputPassword']);
+   $verifInputPassword = checkInput($_POST['verifInputPassword']);
+   if(!empty($_POST['inputPseudo']) AND !empty($_POST['inputMail']) AND !empty($_POST['inputPassword']) AND !empty($_POST['verifInputPassword'])) 
+   {
+      $pseudolength = strlen($inputPseudo);
+      if($pseudolength <= 255) {
+            //Verifie si le email existe déjà dans la bdd
+            if(filter_var($inputMail, FILTER_VALIDATE_EMAIL)) {
+               $reqmail = $bdd->prepare("SELECT * FROM user WHERE email = ?");
+               $reqmail->execute(array($inputMail));
+               $mailexist = $reqmail->rowCount();
+               if($mailexist == 0) {
+                  if($inputPassword == $verifInputPassword) {
+                     $insertMember = $bdd->prepare("INSERT INTO user(pseudo, email, pass, date_inscription) VALUES(?, ?, ?, CURDATE())");
+                     $insertMember->execute(array($inputPseudo, $inputMail, $inputPassword));
+                     $erreur = "Votre compte a bien été créé ! <a href=\"connexion.php\">Me connecter</a>";
+                  } else {
+                     $erreur = "Vos mots de passes ne correspondent pas !";
+                  }
+               } else {
+                  $erreur = "Adresse mail déjà utilisée !";
+               }
+            } else {
+               $erreur = "Votre adresse mail n'est pas valide !";
+            }
+         } 
+      else {
+         $erreur = "Votre pseudo ne doit pas dépasser 255 caractères !";
+      }//else
+   } //if pseudolenght
+   else {
+      $erreur = "Tous les champs doivent être complétés !";
+   }// else
+}//if empty
+?>
+
 
 <?php ob_start(); ?>
 
@@ -9,28 +60,42 @@
 
     <section class="col-lg-6 offset-lg-6" id="connect">
         <h3> Inscrivez-vous </h3>
-        <form action="../back-end/gestionProfil.php" method="post">
+
+        <form action="" method="post">
             <div class="col-lg-8 offset-lg-2">
-                <label class="labelForm" for="inputIdentifiant"> Pseudo </label>
-                <input type="text" class="formInput" id="inputIdentifiant">
-            </div>
-            <div class="col-lg-8 offset-lg-2">
-                <label class="labelForm" for="inputPassword"> Mot de passe </label>
-                <input type="password" class="formInput" id="inputPassword">
-            </div>
-            <div class="col-lg-8 offset-lg-2">
-                <label class="labelForm" for="inputPassword"> Veuillez retaper votre mot de passe </label>
-                <input type="password" class="formInput" id="inputPassword">
+                <label class="labelForm" for="inputPseudo"> Pseudo </label>
+                <input type="text" class="formInput" id="inputPseudo" name="inputPseudo" value="<?php if (isset($_POST['inputPseudo'])) {
+                                                                                                                echo $_POST['inputPseudo'];
+                                                                                                            }  ?>">
             </div>
             <div class="col-lg-8 offset-lg-2">
                 <label class="labelForm" for="inputMail"> Veuillez saisir votre adresse e-mail </label>
-                <input type="password" class="formInput" id="inputMail">
+                <input type="email" class="formInput" id="inputMail" name="inputMail" value="<?php if (isset($_POST['inputMail'])) {
+                                                                                                    echo $_POST['inputMail'];
+                                                                                                }  ?>">
             </div>
+            <div class="col-lg-8 offset-lg-2">
+                <label class="labelForm" for="inputPassword"> Mot de passe </label>
+                <input type="password" class="formInput" id="inputPassword" name="inputPassword">
+            </div>
+            <div class="col-lg-8 offset-lg-2">
+                <label class="labelForm" for="verifInputPassword"> Veuillez retaper votre mot de passe </label>
+                <input type="password" class="formInput" id="verifInputPassword" name="verifInputPassword">
+            </div>
+
             <div class="col-lg-12 text-center" id="buttonConnect">
-                <button class="submit"> Inscription </button>
+                <button class="submit" name="formInscription"> Inscription </button>
             </div>
         </form>
+       <div class="error text-center"> 
+           <?php
+            if (isset($erreur)) {
+                echo $erreur ;
+            }
+            ?>
+       </div>
     </section>
+
 
     <!--******** FOOTER ********-->
     <?php include("includes/footer.php") ?>
