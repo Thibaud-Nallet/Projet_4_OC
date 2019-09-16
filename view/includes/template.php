acd3a681e07dd448266ea9ac27158583f9279a36
Thibaud-Nallet <thibaud.nallet@gmail.com>
2019-09-16 18:10:23 +0200

    Correction balise script

--- view/includes/template.php
+++ view/includes/template.php
@@ -28,13 +28,12 @@
 </head>
 
 <body>
+    
     <?= $content ?>
-</body>
 
 <!--***** SCRIPT *****-->
 <!-- Script Material Design -->
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js">
-</script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"> </script>
 <script src="https://unpkg.com/popper.js@1.12.6/dist/umd/popper.js" integrity="sha384-fA23ZRQ3G/J53mElWqVJEGJzU0sTs+SvzG8fXVWP+kJQ1lwFAOkcUOysnlKJC33U" crossorigin="anonymous">
 </script>
 <script src="https://unpkg.com/bootstrap-material-design@4.1.1/dist/js/bootstrap-material-design.js" integrity="sha384-CauSuKpEqAFajSpkdjv3z9t8E7RlpJ1UP0lKM/+NdtSarroVKu069AlsRPKkFBz9" crossorigin="anonymous">
@@ -50,8 +49,7 @@
         selector: '#mytextarea'
     });
 </script>
-<!-- File js -->
-<script <?= $script ?>> </script>
+
 </body>
 
 </html>
